3b92b0a5a0d6c0c13af300b330eca55dbbb29dc6
jyor <jayocpa@gmail.com>
2023-02-06 23:42:00 +0900

    test

--- home.py
+++ home.py
@@ -47,100 +47,100 @@ st.write("#")
 st.write("### OPEN API: **OPEN DART**")
 
 
-# def get_fs(code, year, cat):
-#     import requests
-#     import pandas as pd
+def get_fs(code, year, cat):
+    import requests
+    import pandas as pd
 
-#     # https://opendart.fss.or.kr/
-#     ENDPOINT = "https://opendart.fss.or.kr/api/fnlttSinglAcntAll.json"
-#     APIKEY = "7491258925ffb7eeeb591110651f67100e3f7125"
-#     params = {
-#         "crtfc_key": APIKEY,
-#         "corp_code": code,
-#         "bsns_year": year,
-#         "reprt_code": "11011",
-#         "fs_div": cat,
-#     }
-#     response = requests.get(ENDPOINT, params=params)
-#     fs_json = json.loads(response.text)
-#     msg = f"{fs_json['status']=!s}, {fs_json['message']=!s}"
-#     st.markdown(
-#         f"""
-#     ```py
-#     {msg}
-#     ```
-#     """
-#     )
-#     if fs_json["status"] == "000":
-#         df = pd.DataFrame(fs_json["list"])
-#         df = df[
-#             [
-#                 "bsns_year",
-#                 "sj_nm",
-#                 "account_id",
-#                 "account_nm",
-#                 "thstrm_nm",
-#                 "thstrm_amount",
-#                 "frmtrm_nm",
-#                 "frmtrm_amount",
-#                 "bfefrmtrm_nm",
-#                 "bfefrmtrm_amount",
-#                 "ord",
-#                 "currency",
-#             ]
-#         ]
-#         df.columns = [
-#             "사업연도",
-#             "재무제표구분",
-#             "계정ID",
-#             "계정명",
-#             "당기",
-#             "당기금액",
-#             "전기",
-#             "전기금액",
-#             "전전기",
-#             "전전기금액",
-#             "순번",
-#             "통화",
-#         ]
+    # https://opendart.fss.or.kr/
+    ENDPOINT = "https://opendart.fss.or.kr/api/fnlttSinglAcntAll.json"
+    APIKEY = "7491258925ffb7eeeb591110651f67100e3f7125"
+    params = {
+        "crtfc_key": APIKEY,
+        "corp_code": code,
+        "bsns_year": year,
+        "reprt_code": "11011",
+        "fs_div": cat,
+    }
+    response = requests.get(ENDPOINT, params=params)
+    fs_json = json.loads(response.text)
+    msg = f"{fs_json['status']=!s}, {fs_json['message']=!s}"
+    st.markdown(
+        f"""
+    ```py
+    {msg}
+    ```
+    """
+    )
+    if fs_json["status"] == "000":
+        df = pd.DataFrame(fs_json["list"])
+        df = df[
+            [
+                "bsns_year",
+                "sj_nm",
+                "account_id",
+                "account_nm",
+                "thstrm_nm",
+                "thstrm_amount",
+                "frmtrm_nm",
+                "frmtrm_amount",
+                "bfefrmtrm_nm",
+                "bfefrmtrm_amount",
+                "ord",
+                "currency",
+            ]
+        ]
+        df.columns = [
+            "사업연도",
+            "재무제표구분",
+            "계정ID",
+            "계정명",
+            "당기",
+            "당기금액",
+            "전기",
+            "전기금액",
+            "전전기",
+            "전전기금액",
+            "순번",
+            "통화",
+        ]
 
-#         df
+        df
 
 
-# with st.expander("예제"):
-#     st.write(
-#         """
-#     ```py
-#     import requests
-#     # https://opendart.fss.or.kr/
-#     ENDPOINT = 'https://opendart.fss.or.kr/api/fnlttSinglAcntAll.json'
-#     APIKEY = 'xxxxxxxxxxxxxxxxxxxxxxxx'
-#     params = {
-#         'crtfc_key': APIKEY,
-#         'corp_code': '00126380',
-#         'bsns_year': '2021',
-#         'reprt_code': '11011',
-#         'fs_div': 'CFS',
-#     }
-#     response = requests.get(ENDPOINT, params=params)
-#     ```
-#     """
-#     )
+with st.expander("예제"):
+    st.write(
+        """
+    ```py
+    import requests
+    # https://opendart.fss.or.kr/
+    ENDPOINT = 'https://opendart.fss.or.kr/api/fnlttSinglAcntAll.json'
+    APIKEY = 'xxxxxxxxxxxxxxxxxxxxxxxx'
+    params = {
+        'crtfc_key': APIKEY,
+        'corp_code': '00126380',
+        'bsns_year': '2021',
+        'reprt_code': '11011',
+        'fs_div': 'CFS',
+    }
+    response = requests.get(ENDPOINT, params=params)
+    ```
+    """
+    )
 
-# with open("corpcode.json", "r", encoding="utf-8") as f:
-#     corp_dict = json.load(f)
+with open("corpcode.json", "r", encoding="utf-8") as f:
+    corp_dict = json.load(f)
 
-# fs_cat_dict = {"연결": "CFS", "개별": "OFS"}
-# col1, col2 = st.columns(2)
+fs_cat_dict = {"연결": "CFS", "개별": "OFS"}
+col1, col2 = st.columns(2)
 
-# with col1:
-#     corp_code = st.selectbox("기업명", options=corp_dict)
-#     fiscal_year = st.selectbox("연도", options=[2021, 2020, 2019, 2018, 2017, 2016, 2015])
-# with col2:
-#     fs_cat = st.selectbox("연결/개별", options=fs_cat_dict)
+with col1:
+    corp_code = st.selectbox("기업명", options=corp_dict)
+    fiscal_year = st.selectbox("연도", options=[2021, 2020, 2019, 2018, 2017, 2016, 2015])
+with col2:
+    fs_cat = st.selectbox("연결/개별", options=fs_cat_dict)
 
-# if st.button(f"{corp_code} 재무제표 가져오기"):
-#     get_fs(corp_dict[corp_code], fiscal_year, fs_cat_dict[fs_cat])
+if st.button(f"{corp_code} 재무제표 가져오기"):
+    get_fs(corp_dict[corp_code], fiscal_year, fs_cat_dict[fs_cat])
 
 
 # with col3:
